e3f2575e826c51334bd64c8156bc372bcf841f4f
Cyrus_The_Great <166158964+06Ayush-Kumar@users.noreply.github.com>
2024-10-11 10:32:17 +0530

    Update 03. Loops.cpp

--- 03. Loops.cpp
+++ 03. Loops.cpp
@@ -1,1 +1,26 @@
+// 4 types of loop:
+// 1. while loop:
+
+while (condition){    // if the conition is true then statement will execute
+    statements to be executes
+}
+
+// 2. do while:: Ideal for case when you want program to run atleast once
+
+do{
+    set of conditions to be executed first (atleast once)
+}
+while(condition to check for){  //repeats the stuff inside do if condition matches
+}    
+
+// 3.for loop
+for(int i=0;i<=4;i++){
+cout<<i;
+}
+
+// for loop for array or vector:
+
+     for (type x : collection/array){  // useful for accessing all the elements of an array.
+    cout<<x;
+}  
 
