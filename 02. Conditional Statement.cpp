49d2bc9043c0498faace6b540f395054eadeb3e4
Cyrus_The_Great <166158964+06Ayush-Kumar@users.noreply.github.com>
2024-10-11 10:25:39 +0530

    Update 02. Conditional Statement.cpp

--- 02. Conditional Statement.cpp
+++ 02. Conditional Statement.cpp
@@ -1,5 +1,35 @@
 
-// if statement :
+// if-else statement :
 
-if(a>b || c>d){
+if(a>b || c>d){  // if a is greater the b or c is greater then d then hi will be print else no will print
 cout<<"hi";
+}
+else{
+cout<<"no";
+}
+
+if(a>=b && c>=e){}  // will be executed only when both are true 
+
+// dynamic declaration:basically limiting the scope of a variable
+
+int main(){
+int a=5;
+{
+int c=6;  // c is availabe only inside this bracket
+}
+cout<<a;  // will print 5
+cout<<b;  // will give error because we cant access c ,Gives an error saying identifier is undefined.
+
+// switch statment:
+switch(day){
+case 1:          // if value of day is 1 then monday will be printed
+cout<<"monday";
+break;
+case 2:
+cout<<"tues";
+break;
+default:       // if 1 and 2 are not input then dafault condiotn will be printed
+cout<<"error";
+break;
+}
+  
